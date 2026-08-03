bb6f510869c1de6eecb9c0d1ab8327e2fc60c384
iTrust <bioitrust0414@gmail.com>
2026-08-03 10:10:05 +0800

    Update bb-drink.index.tsx

--- src/routes/bb-drink.index.tsx
+++ src/routes/bb-drink.index.tsx
@@ -1,26 +1,10 @@
-// src/routes/bb-drink.index.tsx
-import { createFileRoute } from '@tanstack/react-router';
-import { getBBDrinkProducts } from '@/server/bb-drink';
-import { HeroSection } from '@/components/bb-drink/HeroSection';
-import { ProductGrid } from '@/components/bb-drink/ProductGrid';
-import { CartDrawer } from '@/components/bb-drink/CartDrawer';
+import { fetchBBDrinkData } from 'src/server/bb-drink'
 
 export const Route = createFileRoute('/bb-drink/')({
-  component: BBDrinkPage,
   loader: async () => {
-    const products = await getBBDrinkProducts();
-    return { products };
+    // 透過 Server Function 呼叫後端 API
+    const data = await fetchBBDrinkData()
+    return data
   },
-});
-
-function BBDrinkPage() {
-  const { products } = Route.useLoaderData();
-
-  return (
-    <div className="min-h-screen bg-white">
-      <HeroSection />
-      <ProductGrid products={products} />
-      <CartDrawer />
-    </div>
-  );
-}
+  component: BBDrinkPage,
+})
