b02e426a315ed500cfcd18d4d3c08ec6f32af9ba
Thomas Chen <bioitrust0414@gmail.com>
2026-08-08 10:52:20 +0800

    Delete assets.ts

--- src/components/dahua/assets.ts
+++
@@ -1,6 +0,0 @@
-export const clinicImages = [
-  { src: "/dhlp1.webp", alt: "大華醫事檢驗所外觀" },
-  { src: "/dhlp2.webp", alt: "大華醫事檢驗所街景" },
-  { src: "/dhlp3.webp", alt: "大華醫事檢驗所前台" },
-  { src: "/dhlp4.webp", alt: "大華醫事檢驗所內部" },
-];
