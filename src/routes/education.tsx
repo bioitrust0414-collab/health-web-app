26920e7962083c1c21c48bf6559800af36481d7b
Thomas Chen <bioitrust0414@gmail.com>
2026-08-08 10:53:45 +0800

    Delete education.tsx

--- src/routes/education.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import dahuaCss from "@/styles/dahua.css?url";
-
-export const Route = createFileRoute("/education")({
-  head: () => ({
-    links: [{ rel: "stylesheet", href: dahuaCss }],
-  }),
-  component: () => <Outlet />,
-});
