47ef5c88fbad600214c86ccdf5c61241bf87bd40
iTrust <bioitrust0414@gmail.com>
2026-08-08 09:29:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './client/src'),
     },
   },
-  root: './client',
+  root: path.resolve(__dirname, 'client'),
   build: {
-    outDir: '../dist',
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
   server: {
