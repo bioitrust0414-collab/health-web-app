277513e35cfa27edc3872f2fdd594bb5511a9a36
iTrust <bioitrust0414@gmail.com>
2026-08-10 14:32:18 +0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import Navbar from "@/components/Navbar";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  ),
+  component: () => <Outlet />,
 });
