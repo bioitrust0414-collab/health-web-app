c413b087fc4954ad11efb732539b33a04f9629c6
iTrust <bioitrust0414@gmail.com>
2026-08-10 15:31:06 +0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
+// src/routes/__root.tsx
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import "@/styles/dahua.css";   // ← 全局樣式，所有路由自動繼承
 
 export const Route = createRootRoute({
   component: () => <Outlet />,
